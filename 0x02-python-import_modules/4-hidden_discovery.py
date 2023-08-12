35de983cb5aa97039c84dcfb544ab4831abb28a1
Yimiika <oluwafemiaina8@gmail.com>
2023-08-12 05:43:05 -0700

    coding

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ import hidden_4
 if __name__ == "__main__":
     names = dir(hidden_4)
     sortednames = sorted([name for name in names if not name.startswith("__")])
-    
+
     for name in sortednames:
         print(name)
