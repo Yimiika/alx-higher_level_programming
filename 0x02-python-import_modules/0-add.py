0a8100306ceb1ae2f92cbb3d53eb89273725ee64
Yimiika <oluwafemiaina8@gmail.com>
2023-08-12 04:04:44 -0700

    coding

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+
+"""This program imports the function `add` from the file `add_0.py` and prints the result of the addition `1 + 2 = 3`.
+
+The code complies with the pycodestyle style guide (version 2.8.*).
+"""
+
 from add_0 import add
 
 a = 1
 b = 2
-print(f"{a} + {b} = {add(a,b)}")
+
+print(f"{a} + {b} = {add(a, b)}")
